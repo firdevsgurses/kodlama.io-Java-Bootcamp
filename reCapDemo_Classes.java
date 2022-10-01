e75c101c60671416c014eb07ced724d4529fb968
Firdevs Gürses <74095639+firdevsgurses@users.noreply.github.com>
2022-10-01 15:08:48 +0300

    Delete reCapDemo_Classes.java

--- reCapDemo_Classes.java
+++
@@ -1,9 +0,0 @@
-
-public class reCapDemo_Classes {
-
-	public static void main(String[] args) {
-		operations operation = new operations();
-		int result = operation.sum(5, 8);
-		System.out.println(result);
-	}
-}
